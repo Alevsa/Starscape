025346f778d26ba0b747c8a45ef421b7428ec858
JohnFrogman <Alex_R_HAyes@hotmail.co.uk>
2015-10-03 17:01:48 +0100

    Realised I'm a retard
    There's no problem with dropping out warp because charging warp already
    immediately puts you back to next to zero speed.
    
    Also there isn't a reason to simulate circular motion when you can just
    use RotateAround. I'm keeping a hold of my old script though cause it's
    great.

--- Starscape/Assets/Code/Overworld/Environment/CelestialOrbit.cs
+++ Starscape/Assets/Code/Overworld/Environment/CelestialOrbit.cs
@@ -4,61 +4,28 @@ using System.Collections;
 public class CelestialOrbit : MonoBehaviour 
 {
 	public GameObject focus;
-	private GameObject pointer;
-	private float orbitalRadius;
-	private Rigidbody body;
-	public float period = 10f;
-	private float speed;
+	public bool clockwise;
+	public float orbitalPeriod = 60f;
+	private float orbitalSpeed = 360f/60f;
+	private Vector3 axis = Vector3.down;
 	
 	void Start()
 	{
-		body = gameObject.GetComponent<Rigidbody>();
-		orbitalRadius = Vector3.Distance(transform.position, focus.transform.position);
-		speed = 2f * Mathf.PI * orbitalRadius / period;		
-		#region Gets the pointer
-		for (int i = 0; i<transform.childCount; i++)
-		{	
-			GameObject obj = transform.GetChild(i).gameObject;
-			if (obj.tag == "Pointer")
-			{
-				pointer = obj;
-			}
+		if (clockwise)
+		{
+			axis = Vector3.up;
 		}
-		#endregion
-	}
-	// Update is called once per frame
-	void FixedUpdate () 
-	{
-		move();
-	
-	}
-	#region This whole thing is kind of disjointed cause I realised different things were required at different times. 
-
-	void move()
-	{
-		centripetalForce();
-		velocity();
-		gameObject.transform.position = gameObject.transform.position;
-		
+		else 
+		{
+			axis = Vector3.down;
+		}
+		orbitalSpeed = 360f/orbitalPeriod;
 	}
 	
-	// Applies centripetal force
-	void centripetalForce()
+	void Update()
 	{
-		pointer.transform.LookAt(focus.transform);
-		Vector3 forwardForce = pointer.transform.forward * Mathf.Pow(speed, 2) * body.mass / orbitalRadius;
-		body.AddRelativeForce (forwardForce);
+		transform.RotateAround(focus.transform.position, axis, orbitalSpeed*Time.deltaTime);
 	}
-	// force in the direction of motion
-	void velocity()
-	{
-		
-		pointer.transform.LookAt(focus.transform);
-		pointer.transform.Rotate(new Vector3(0f, 90f, 0f)) ;
-		Vector3 forwardForce = pointer.transform.forward * speed * body.mass;
-		body.AddRelativeForce (forwardForce);
-	}
-	#endregion
 
 
 }
