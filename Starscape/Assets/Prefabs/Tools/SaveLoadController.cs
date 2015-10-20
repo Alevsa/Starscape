2218568bdf7bd89711a16251fa58d4b448dcd2e3
Comeonyougit <jakedesu@gmail.com>
2015-10-20 19:33:00 +0300

    SaveLoad static class that you can hook up anywhere

---
+++ Starscape/Assets/Prefabs/Tools/SaveLoadController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class SaveLoadController {
+
+	public static void SavePlayerPosition (Vector3 pos)
+	{
+		PlayerPrefs.SetFloat ("pXpos", pos.x);
+		PlayerPrefs.SetFloat ("pYpos", pos.y);
+		PlayerPrefs.SetFloat ("pZpos", pos.z);
+	}
+
+	public static Vector3 GetSavedPlayerPosition ()
+	{
+		Vector3 res;
+		res.x = PlayerPrefs.GetFloat ("pXpos");
+		res.y = PlayerPrefs.GetFloat ("pYpos");
+		res.z = PlayerPrefs.GetFloat ("pZpos");
+
+		return res;
+	}
+
+	public static void SavePlayerHealth (float health)
+	{
+		PlayerPrefs.SetFloat ("pHealth", health);
+	}
+
+	public static float GetPlayerHealth ()
+	{
+		float res = PlayerPrefs.GetFloat ("pHealth");
+
+		return res;
+	}
+}
