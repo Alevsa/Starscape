95bc96dc0abef29544e7712d7df2a41a0b025e75
Sam Rowlands <srowlands@hotmail.co.uk>
2015-11-08 18:14:40 +0000

    Finalised the laser weapon, preddy c00l

--- Starscape/Assets/Code/Battle/Weapons/WeaponScripts/PlayerLaserWeapon.cs
+++ Starscape/Assets/Code/Battle/Weapons/WeaponScripts/PlayerLaserWeapon.cs
@@ -6,8 +6,8 @@ public class PlayerLaserWeapon : LaserWeapon
     public override Vector3 SetDirection(Transform firingPoint)
     {
         Vector3 dir = firingPoint.forward;
-        dir += firingPoint.right * Input.GetAxis("Horizontal");
-        dir += firingPoint.up * Input.GetAxis("Vertical");
+        dir += firingPoint.right * (Input.GetAxis("Horizontal")/10);
+        dir += firingPoint.up * (Input.GetAxis("Vertical")/10);
         dir.Normalize();
         return dir;
     }
