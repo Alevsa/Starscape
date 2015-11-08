32c85f3b0c7870a31b94ba6c69779309d8ef756e
Sam Rowlands <srowlands@hotmail.co.uk>
2015-11-08 22:55:36 +0000

    Tweaks

--- Starscape/Assets/Code/Battle/Weapons/WeaponScripts/PlayerLaserWeapon.cs
+++ Starscape/Assets/Code/Battle/Weapons/WeaponScripts/PlayerLaserWeapon.cs
@@ -6,8 +6,8 @@ public class PlayerLaserWeapon : LaserWeapon
     public override Vector3 SetDirection(Transform firingPoint)
     {
         Vector3 dir = firingPoint.forward;
-        dir += firingPoint.right * (Input.GetAxis("Horizontal")/7);
-        dir += firingPoint.up * (Input.GetAxis("Vertical")/7);
+        dir += firingPoint.right * (Input.GetAxis("Horizontal")/9);
+        dir += firingPoint.up * (Input.GetAxis("Vertical")/9);
         dir.Normalize();
         return dir;
     }
