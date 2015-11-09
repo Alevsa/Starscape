c19589dc64cf9cbc3c50d0b02fea322f081b4ff4
Sam Rowlands <srowlands@hotmail.co.uk>
2015-11-09 16:54:38 +0000

    Some main menu fixes, when creating a new game.

---
+++ Starscape/Assets/Code/Menu/HandleText.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HandleText : MonoBehaviour
+{
+    public void HidePlaceholder(GameObject placeholder)
+    {
+        placeholder.GetComponent<Text>().text = "";
+    }
+}
