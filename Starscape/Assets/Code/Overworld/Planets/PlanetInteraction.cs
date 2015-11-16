756ba7b692c305a74e8550356d6b51c3b958e8f5
Sam Rowlands <srowlands@hotmail.co.uk>
2015-11-16 18:04:44 +0000

    Worked on the planet interaction in overworld.

---
+++ Starscape/Assets/Code/Overworld/Planets/PlanetInteraction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetInteraction : MonoBehaviour
+{
+    public GameObject m_EnterPanel;
+    private UpdateText m_EnterText;
+
+    void Start()
+    {
+        m_EnterText = m_EnterPanel.transform.FindChild("EnterText").GetComponent<UpdateText>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            m_EnterText.SetText(gameObject.name);
+            m_EnterPanel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            m_EnterPanel.SetActive(false);
+        }
+    }
+}
---
+++ Starscape/Assets/Code/Overworld/Planets/UpdateText.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpdateText : MonoBehaviour
+{
+    public string DisplayMessage = "Dock with ";
+
+    public void SetText(string text)
+    {
+       GetComponent<Text>().text = DisplayMessage + text;
+    }
+}
