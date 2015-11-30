77f08ea95aa46379da1902ea08b5b8ca3a745a5a
Alex Hayes <Alex_R_HAyes@hotmail.co.uk>
2015-11-30 22:14:19 +0000

    Minor changes to the stage director

--- Starscape/Assets/Code/Overworld/Story/StageDirector.cs
+++ Starscape/Assets/Code/Overworld/Story/StageDirector.cs
@@ -4,10 +4,22 @@ using System.Collections;
 public class StageDirector : MonoBehaviour
 {
     private int m_ActiveStage;
-    public GameObject MissionObjects[];
+    public GameObject[] MissionObjects;
 
     void Start ()
     {
         m_ActiveStage = SaveLoadController.GetStoryStage();
 	}
+
+    void setActiveMissions()
+    {
+        foreach (GameObject obj in MissionObjects)
+        {
+            obj.SetActive(false);
+        }
+        for (int i = 0; i < m_ActiveStage; i++)
+        {
+            MissionObjects[i].SetActive(true);
+        }
+    }
 }
