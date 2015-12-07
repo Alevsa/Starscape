f172312833e999e8414409a2dbeb2ec2888bbb99
Sam Rowlands <srowlands@hotmail.co.uk>
2015-12-07 23:18:12 +0000

    Fixed main menu animations and text spacing. Added panels to planet interface.

---
+++ Starscape/Assets/Code/Overworld/Planets/PlanetUIPanels.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetUIPanels : MonoBehaviour
+{
+    public GameObject[] Panels;
+
+    public void ChangePanel(GameObject panel)
+    {
+        foreach (GameObject pan in Panels)
+        {
+            pan.SetActive(false);
+        }
+
+        panel.SetActive(true);
+    }
+}
