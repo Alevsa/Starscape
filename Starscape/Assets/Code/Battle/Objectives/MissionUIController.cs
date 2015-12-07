7bbdce655de9b91226051b037197911adc6b8778
Alex Hayes <Alex_R_HAyes@hotmail.co.uk>
2015-12-07 22:43:06 +0000

    Fixed mission ui control
    Buttons need to work next.

--- Starscape/Assets/Code/Battle/Objectives/MissionUIController.cs
+++ Starscape/Assets/Code/Battle/Objectives/MissionUIController.cs
@@ -12,18 +12,26 @@ public class MissionUIController : MonoBehaviour
 
     public void Win(string winText)
     {
-        TimeStop();
-        WinUI.SetActive(true);
-        WinText.text = winText;
+        if (inProgress)
+        {
+            inProgress = false;
+            TimeStop();
+            WinUI.SetActive(true);
+            WinText.text = winText;
+        }
     }
 
     public void Lose(string[] loseText)
     {
-        TimeStop();
-        LoseUI.SetActive(true);
-        foreach (string x in loseText)
+        if (inProgress)
         {
-            LoseText.text += x;
+            inProgress = false;
+            TimeStop();
+            LoseUI.SetActive(true);
+            foreach (string x in loseText)
+            {
+                LoseText.text += x;
+            }
         }
     }
 
