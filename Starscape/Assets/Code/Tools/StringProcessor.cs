de409836155d22f18072b21c7fa862541aec6583
Alex Hayes <Alex_R_HAyes@hotmail.co.uk>
2015-12-17 22:50:37 +0000

    Started on a string processor
    This will process strings to replace variables, like the player's name
    and controls.

---
+++ Starscape/Assets/Code/Tools/StringProcessor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+public static class StringProcessor : MonoBehaviour
+{
+    // Horizontal @JOY+
+    // Vertical @JOY-
+    // Stabilise @STAB
+    // Hand Brake @BRAKE
+    // Roll @ROLL
+    // Rearview @REAR
+    // BattleAccelerate @ACC
+    // Fire @FIRE
+
+    public static string ProcessMissionString(string input)
+    {
+        string output = input;
+        return output;
+    }
+
+    private static string InterpretBattleControls(string input)
+    {
+        string output = input;
+        output.Replace("@JOY", "Joystick");
+        Input.
+        return output;
+    }
+}
