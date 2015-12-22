420f59f8b69f2acd7e49320e68ec36ec94e02010
Alex Hayes <Alex_R_HAyes@hotmail.co.uk>
2015-12-22 22:38:10 +0000

    Race objective

--- Starscape/Assets/Code/Battle/Objectives/Objectives/RaceObjective.cs
+++ Starscape/Assets/Code/Battle/Objectives/Objectives/RaceObjective.cs
@@ -4,13 +4,22 @@ using System;
 
 public class RaceObjective : Objective
 {
+    public Transform Racer;
+    public Collider FinishLine;
+
     public override void CheckCompletion()
     {
-        throw new NotImplementedException();
+        if (FinishLine.bounds.Contains(Racer.position))
+        {
+            Completed = true;
+        }
     }
 
     public override void CheckFailure()
     {
-        throw new NotImplementedException();
+        if (TimeLimit < 0f && m_IsTimed)
+        {
+            Failed = true;
+        }
     }
 }
