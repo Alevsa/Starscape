a7a6bd309b9e52b06f83963cfc0f39f06ed6ac45
Alex Hayes <Alex_R_HAyes@hotmail.co.uk>
2015-12-28 22:32:18 +0000

    Out of bounds penalty implemented partially.

---
+++ Starscape/Assets/Code/Battle/Objectives/MissionBounds.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MissionBounds : MonoBehaviour
+{
+    private Bounds m_Bounds;
+    public Transform Player;
+    private ShipCore m_PlayerCore;
+    public GameObject OutOfBoundsWarning;
+    private bool m_OutOfBounds;
+    private Coroutine m_Countdown;
+    public float TimeToDie = 5f;
+
+    void Start()
+    {
+        m_PlayerCore = Player.GetComponent<ShipCore>();
+        m_Bounds = gameObject.GetComponent<Collider>().bounds;
+    }
+
+    void Update()
+    {
+        if (!m_Bounds.Contains(Player.position) )
+        {
+            if (!m_OutOfBounds)
+            {
+                m_Countdown = StartCoroutine("OutOfBoundsCountdown");
+            }
+            m_OutOfBounds = true;
+        }
+        else
+        {
+            m_OutOfBounds = false;
+        }
+        SetWarningTextActivity(m_OutOfBounds);
+
+    }
+
+    private void SetWarningTextActivity(bool x)
+    {
+        OutOfBoundsWarning.SetActive(x);
+    }
+
+    IEnumerator OutOfBoundsCountdown()
+    {
+        for (float i = 0f; i < TimeToDie; i += Time.deltaTime)
+        {
+            yield return null;
+        }
+        m_PlayerCore.Health -= m_PlayerCore.MaxHealth;
+    }
+
+}
