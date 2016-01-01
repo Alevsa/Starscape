0e8397843c5c0b0f1695210903f05e5358a7c429
Alex Hayes <Alex_R_HAyes@hotmail.co.uk>
2016-01-01 08:25:07 +0000

    Dialogue system functionally complete
    But it hasn't been tested or polished, it also can't be triggered by
    anything yet.

--- Starscape/Assets/Code/Dialogue/DialogueController.cs
+++ Starscape/Assets/Code/Dialogue/DialogueController.cs
@@ -8,13 +8,14 @@ public class DialogueController : MonoBehaviour
     private XmlDocument m_DialogueChain;
     public string DialoguePath = "TestDialogue.xml";
     public bool OverworldDialgue = false;
-    public float DialogueSpeed = 1f;
+    public float DialogueSpeed = 2f;
     private XmlNodeList m_DialogueLines;
     public GameObject DialogueUI;
     public Image CharacterPortrait;
     public Text DialogueText;
     private bool m_DrawingText = false;
-
+    public float Delay = 2f;
+    public Text NameText;
 
     void Start()
     {
@@ -28,48 +29,39 @@ public class DialogueController : MonoBehaviour
     public void PlayDialogue()
     {
         DialogueUI.SetActive(true);
-        foreach (XmlNode node in m_DialogueLines)
-        {
-            PlayDialogueLine(node);
-        }
-        //DialogueUI.SetActive(false);
+        StartCoroutine("PlayDialogueLine", 0);
         Resources.UnloadUnusedAssets();
     }
 
-    void PlayDialogueLine(XmlNode x)
+    IEnumerator PlayDialogueLine(int index)
     {
+        XmlNode x = m_DialogueLines[index];
         string text = x.InnerText;
         string portrait = x.Attributes["portrait"].Value;
-        //portrait = "Art/Portraits/" + portrait;
-        string name = x.Attributes["speakerName"].Value;
+        portrait = "Art/Portraits/" + portrait;
         Sprite img = Resources.Load<Sprite>(portrait);
         Debug.Log(img); Debug.Log(portrait);
-
         CharacterPortrait.sprite = img;
-
         DialogueText.text = text;
-        //StartCoroutine("PlayText", text);
-        
-        // Delay before next stage
-        //DialogueText.text = "";
-        //CharacterPortrait.sprite = null;
-    }
+        NameText.text = x.Attributes["speakerName"].Value;
 
-    IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(3f);
-    }
-
-    IEnumerator PlayText(string line)
-    {
-        for (int i = 0; i < line.Length; i++)
+        for (int i = 0; i < text.Length; i++)
         {
-            for (float j = 0; j < DialogueSpeed; j += Time.deltaTime)
-            {
-                DialogueText.text = line.Substring(0, i + 1);
-                Debug.Log(DialogueText.text);
-                yield return null;
-            }
+            DialogueText.text = text.Substring(0, i + 1);
+            yield return new WaitForSeconds(DialogueSpeed);
+        }
+        yield return new WaitForSeconds(Delay);
+        DialogueText.text = "";
+        CharacterPortrait.sprite = null;
+        NameText.text = "";
+
+        if (m_DialogueLines[index + 1] != null)
+        {
+            StartCoroutine("PlayDialogueLine", index + 1);
+        }
+        else
+        {
+            DialogueUI.SetActive(false);
         }
     }
 }
