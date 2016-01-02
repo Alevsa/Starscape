8f6cd456da144d84cf7a47d74ec87b0226c8f69a
Alex Hayes <Alex_R_HAyes@hotmail.co.uk>
2016-01-02 22:54:05 +0000

    Despawn event and demo mission started.

---
+++ Starscape/Assets/Code/Battle/Objectives/Events/TimeDespawnEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TimeDespawnEvent : TimedEvent
+{
+    public GameObject[] Despawns;
+    public override void Fire()
+    {
+        foreach (GameObject Despawn in Despawns)
+            Despawn.SetActive(false);
+    }
+
+}
